4c9ef4f5ab1ad41e198ef37ab6db7b5d3bfda55f
Technical Savage <168007796+Nalin2k@users.noreply.github.com>
2025-06-27 22:42:20 +0530

    update

--- src/Main/LibraryManagementSystem.java
+++ src/Main/LibraryManagementSystem.java
@@ -7,7 +7,7 @@ public class LibraryManagementSystem {
 
 	public static void main(String[] args) {
 		Library library = new Library();
-		
+		 
 		Book book1 = new Book("ISBN001", "Rich Dad Poor Dad", "Robert T. Kiyosaki");
 		Book book2 = new Book("ISBN002", "The Monk Who Sold his Ferrari", "Robin Sharma");   
 		
